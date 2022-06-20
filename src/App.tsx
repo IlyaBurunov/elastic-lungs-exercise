f1da5afe661a120a9712693d667b92bcbbd3eb0e
Ilya Burunov <burunov1714@gmail.com>
2022-06-20 18:21:37 +0300

    refactor(core): add types

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { ExercisePage } from './ExercisePage';
+
+function App() {
+  return <ExercisePage />;
+}
+
+export default App;
